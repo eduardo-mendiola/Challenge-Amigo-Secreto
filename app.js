1015867c01e305097a2ba0cea95477464880ed67
ezequielmz <93398008+ezequielmz@users.noreply.github.com>
2025-01-26 20:50:51 -0300

    Se guardan y muestran los nombres de los amigos

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+let amigoIngresado = "";
+
+function agregarAmigo() {
+  amigoIngresado = document.getElementById("amigo").value;
+  if (amigoIngresado === "") {
+    alert("Por favor, inserte un nombre.");
+  } else {
+    amigos.push(amigoIngresado);
+    limpiarCampo();
+    mostrarListaAmigos();
+  }
+}
+
+function limpiarCampo() {
+  document.getElementById("amigo").value = "";
+}
+
+function crearListaHTML() {
+  let listaHTML = "";
+  for (let amigo of amigos) {
+    listaHTML += `<li>${amigo}</li>`;
+  }
+  return listaHTML;
+}
+
+function mostrarListaAmigos() {
+  let elementoHtml = document.getElementById("listaAmigos");
+  elementoHtml.innerHTML = crearListaHTML();
+}
+
